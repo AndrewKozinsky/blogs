c0378c9f56f5d2579f383cb63354653902d92c6f
Andrew Kozinsky <andkozinskiy@yandex.ru>
2024-02-21 08:32:21 +0500

    Fix 25

--- api/src/config/config.ts
+++ api/src/config/config.ts
@@ -2,8 +2,7 @@ export const config = {
 	port: 3000,
 	refreshToken: {
 		name: 'refreshToken',
-		// 999 удалить потом
-		lifeDurationInMs: 1000 * 20 * 999, // 20 seconds
+		lifeDurationInMs: 1000 * 20, // 20 seconds
 	},
 	accessToken: {
 		name: 'accessToken',
